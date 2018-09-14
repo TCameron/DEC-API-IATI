c29f2a95b516c65f25d06eb886225991910514e6
Timothy Cameron <TCameron@users.noreply.github.com>
2018-09-14 16:05:31 -0400

    Update csv_script_iati.py
    Hard coded the code list to cut out extraneous attempts at downloading files.

--- csv_script_iati.py
+++ csv_script_iati.py
@@ -3,38 +3,48 @@ import urllib.parse
 import shutil
 import base64
 import glob
-import os
-import time
-import datetime
+import csv
 from datetime import date
-
 
 __author__ = "Timothy Cameron"
 __email__ = "tcameron@devtechsys.com"
-__date__ = "06-15-2017"
-__version__ = "0.1"
-now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]+'Z'
+__date__ = "09-14-2018"
+__version__ = "0.3"
+
+
+def globit():
+    interesting_files = glob.glob("*.csv")
+
+    header_saved = False
+    with open('output.csv', 'wb') as fout:
+        for filename in interesting_files:
+            with open(filename) as fin:
+                header = next(fin)
+                if not header_saved:
+                    fout.write(bytes(header, 'UTF-8'))
+                    header_saved = True
+                for line in fin:
+                    fout.write(bytes(line, 'UTF-8'))
 
 
 # Method to combine the data downloaded from the DEC website into a single file
 def concat(firstyear, lastyear):
     print("Combining files...")
+    filename = ""
     for j in range(firstyear, (lastyear+1)):
-        filename = "export/" + time.strftime("%m-%d-%Y") + "/DEC_Data_From_" + str(j) + ".csv"
+        filename = "DEC_Data_From_" + str(j) + ".csv"
         with open(filename) as fin:
-            conloop(fin, 1, firstyear, lastyear)
+            conloop(fin, 1)
     print("finished")
     exit(0)
 
 
-# Concats files, row by row, into one CSV file
-def conloop(infile, line, f, l):
-    outputfilename = "export/" + time.strftime("%m-%d-%Y") + "/DEC_Data_From_" + str(f) + "-" + str(l) + ".csv"
+def conloop(infile, line):
+    outputfilename = "DEC_Data_From_" + str(first) + "-" + str(last) + ".csv"
     files = glob.glob('*.csv')
     with open(outputfilename, 'w') as fout:
         for file_ in files:
             for entry in open(file_, 'r'):
-                print(entry)
                 try:
                     fout.write(entry)
                     line += 1
@@ -42,8 +52,6 @@ def conloop(infile, line, f, l):
                     conloop(infile, line+1)
 
 
-# TODO: Find a use for this function? I believe it was to remove header lines from concat'd files
-# Replaces a specific line within a file with a given string
 def replace_line(filename, line_num, text):
     lines = open(filename, 'r').readlines()
     lines[line_num] = text
@@ -52,56 +60,52 @@ def replace_line(filename, line_num, text):
     out.close()
 
 
-# Error checking and correction for the input years
-def check_years(first_year, last_year):
-    if last_year < first_year:
-        temp = first_year
-        first_year = last_year
-        last_year = temp
-    if last_year < 1953:
-        last_year = 1953
-    elif last_year > date.today().year:
-        last_year = date.today().year
-        print('Cannot search for dates in the future. Setting last year to: ' + str(last_year))
-    return first_year, last_year
-
-
-# Grab the user's parameters
-first = int(input("Input the first year of the range: "))
-last = int(input("Input the last year of the range: "))
-
-# Fix the years, in case of an error
-first_corrected, last_corrected = check_years(first, last)
-
-# If the user answers y or yes, then concat data and end program; Otherwise, download files
-skip = input("Would you like combine pre-existing files? [Y/N] ")
+first = int(input("Input the first year to search for: "))
+last = int(input("Input the last year to search for: "))
+skip = input("Would you like combine preexisting files? [Y/N] ")
 if skip.lower() == 'y' or skip.lower() == 'yes':
-    concat(first_corrected, last_corrected)
-# Download data; if the user enters y or yes, concat the data after download
-answer = ''
-if first_corrected != last_corrected:
-    answer = input("Would you like to combine the downloaded files? [Y/N] ")
+    # concat(first, last)
+    globit()
 
-# Make download directory
-if not os.path.exists("export/" + time.strftime("%m-%d-%Y") + "/"):
-    os.makedirs("export/" + time.strftime("%m-%d-%Y") + "/")
+# If the user answers y or yes, then concat data; Otherwise, end program
+if first != last:
+    answer = input("Would you like to combine the downloaded files? [Y/N] ")
+else:
+    answer = 'n'
+
+# Error checking and correction
+if last < first:
+    temp = first
+    first = last
+    last = temp
+
+if last < 1953:
+    last = 1953
+elif last > date.today().year:
+    last = date.today().year
+    print('Cannot search for dates in the future. Setting last year to: ' + str(last))
+
 
 # Main loop to download the data
-for i in range(first_corrected, (last_corrected+1)):
-    print("Downloading data for the year " + str(i) + "...")
-    search = 'documents.date_of_publication_freeforrm:(' + str(i) \
-             + ') AND documents.bibliographic_type_code=(21 OR 22 OR 24)'
-    byte_string = search.encode('utf-8')
-    conv = base64.b64encode(byte_string)
-    converted = str(conv).replace('b\'', '')
-    converted = converted.replace('\'', '')
-    converted = urllib.parse.quote_plus(converted)
-    urltoopen = "https://dec.usaid.gov/api/qsearch.ashx?q=" + converted + "&rtype=CSV"
-    file_name = "export/" + time.strftime("%m-%d-%Y") + "/DEC_Data_From_" + str(i) + ".csv"
-    with urllib.request.urlopen(urltoopen) as response, open(file_name, 'wb') as out_file:
-        shutil.copyfileobj(response, out_file)
-    print("Data for the year " + str(i) + " has finished downloading.")
+for i in range(first, (last+1)):
+    codelist = [11, 12, 13, 14, 15, 16, 21, 22, 24, 25, 26, 27, 31, 32,
+                33, 34, 35, 41, 42, 43, 44, 45, 46, 47, 48, 51, 52, 53, 54, 55, 56, 61, 62, 63]
+    for j in codelist:
+            print("Downloading data for the year " + str(i) + " and type " + str(j) + "...")
+            search = 'documents.date_of_publication_freeforrm:(' + str(i) \
+                     + ') AND documents.bibliographic_type_code=(' + str(j) + ')'
+            byte_string = search.encode('utf-8')
+            conv = base64.b64encode(byte_string)
+            converted = str(conv).replace('b\'', '')
+            converted = converted.replace('\'', '')
+            converted = urllib.parse.quote_plus(converted)
+            urltoopen = "https://dec.usaid.gov/api/qsearch.ashx?q=" + converted + "&rtype=CSV"
+            file_name = "DEC_Data_From_" + str(i) + " " + str(j) + " Group.csv"
+            with urllib.request.urlopen(urltoopen) as response, open(file_name, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+            print("Data for the year " + str(i) + " has finished downloading.")
 
 # Run the concatenation at this point, after all of the data has been downloaded
 if answer.lower() == 'y' or answer.lower() == 'yes':
-    concat(first_corrected, last_corrected)
+    # concat(first, last)
+    globit()
